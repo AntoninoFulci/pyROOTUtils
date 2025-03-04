533e1a791512f206f97c01da003d0a44032c1ef1
AntoninoFulci <antoninofulci@gmail.com>
2025-03-04 15:37:04 +0100

    Multiple stiles for different th

--- article_style.py
+++ article_style.py
@@ -1,52 +1,76 @@
 import ROOT
+import numpy as np
 
-def article_style():
-    ArticleStyle = ROOT.TStyle('ArticleStyle','Article Style by AF')
-        
-    ArticleStyle.SetOptStat(0)
-    ArticleStyle.SetOptTitle(0)
-
-    #Per salvare le immagini in png in alta risoluzione
-    ArticleStyle.SetImageScaling(3.)
-
-    #Canvas options
-    ArticleStyle.SetCanvasDefW(500)
-    ArticleStyle.SetCanvasDefH(500)
-    ArticleStyle.SetCanvasColor(0)
-    ArticleStyle.SetTitleFillColor(0)
-    ArticleStyle.SetStatColor(0)
-
-    #Pad options
-    ArticleStyle.SetPadBottomMargin(0.12) # Margins
-    ArticleStyle.SetPadTopMargin(0.08)
-    ArticleStyle.SetPadLeftMargin(0.125)
-    ArticleStyle.SetPadRightMargin(0.145)
-
-    #Frame options
-    ArticleStyle.SetFrameLineWidth(2)
-
-    #Palette options
-    ArticleStyle.SetPalette(55,0) # avoid default color scheme
-
-    #Title options
-    ArticleStyle.SetTitleSize(0.043,"xyz")   # size of axis title font
-    ArticleStyle.SetTitleSize(0.05,"")
-    ArticleStyle.SetTitleFont(132,"")     # font option
-    ArticleStyle.SetTitleFont(132,"xyz")    # font option
-    ArticleStyle.SetTitleOffset(1.4, "y")
-    ArticleStyle.SetTitleOffset(1.25, "x")
-
-    #Label options
-    ArticleStyle.SetLabelSize(0.04,"xyz")  # size of axis value font
-    ArticleStyle.SetLabelFont(132,"xyz")
-
-    # #Legend options
-    ArticleStyle.SetLegendBorderSize(0)
-    ArticleStyle.SetLegendFont(132)
-    ArticleStyle.SetLegendFillColor(-1)
-
-    #Other
-    ArticleStyle.SetLineWidth(2) #make tick tickier
-    ArticleStyle.SetFillStyle(4000)
+def general_style():
+    """Define the general style settings."""
+    style = ROOT.TStyle('GeneralStyle', 'General Style by AF')
+    # Disable statistics box and title display
+    style.SetOptStat(0)
+    style.SetOptTitle(0)
     
-    return ArticleStyle
+    # Set image scaling for high-resolution PNGs
+    style.SetImageScaling(2.)
+    
+    # Canvas options
+    style.SetCanvasDefW(500)
+    style.SetCanvasDefH(500)
+    style.SetCanvasColor(0)
+    style.SetTitleFillColor(0)
+    style.SetStatColor(0)
+    
+    # Pad options
+    style.SetPadBottomMargin(0.12)
+    style.SetPadTopMargin(0.08)
+    style.SetPadLeftMargin(0.125)
+    
+    # Frame options
+    style.SetFrameLineWidth(1)
+    
+    # Palette options
+    style.SetPalette(55, 0)
+    
+    # Title options
+    style.SetTitleSize(0.043, "xyz")
+    style.SetTitleSize(0.05, "")
+    style.SetTitleFont(132, "")
+    style.SetTitleFont(132, "xyz")
+    style.SetTitleOffset(1.4, "y")
+    style.SetTitleOffset(1.25, "x")
+    
+    # Label options
+    style.SetLabelSize(0.04, "xyz")
+    style.SetLabelFont(132, "xyz")
+    
+    # Legend options
+    style.SetLegendBorderSize(0)
+    style.SetLegendFont(132)
+    style.SetLegendFillColor(-1)
+    
+    # Other options
+    style.SetLineWidth(2)
+    style.SetFillStyle(4000)
+    return style
+
+def th1_style():
+    """Define the style settings for TH1 histograms."""
+    style = general_style()
+    style.SetName('TH1Style')
+    
+    # Custom palette
+    hex_colors = ["#3f90da", "#ffa90e", "#bd1f01", "#94a4a2", "#832db6", "#a96b59", "#e76300", "#b9ac70", "#92dadd"]
+    colors = [ROOT.TColor.GetColor(hex) for hex in hex_colors]
+    myPalette = np.array(colors, dtype=np.int32)
+    style.SetPalette(9, myPalette)
+    
+    # Adjust right margin for TH1
+    style.SetPadRightMargin(0.05)
+    return style
+
+def th2_style():
+    """Define the style settings for TH2 histograms."""
+    style = general_style()
+    style.SetName('TH2Style')
+    
+    # Adjust right margin for TH2 to accommodate color bar
+    style.SetPadRightMargin(0.145)
+    return style
--- root_set_style.py
+++ root_set_style.py
@@ -1,20 +1,21 @@
 import ROOT
 
-
 class set_style():
-    
-    def __init__(self, style=ROOT.TStyle("Modern","Modern Style")):
+    def __init__(self, style=ROOT.TStyle("Modern", "Modern Style")):
         self.style = style
-        
+
     def __enter__(self):
+        # Save the current style name
         self.old_style = ROOT.gStyle.GetName()
         
+        # Set the new style
         global myStyle
         myStyle = self.style
-        ROOT.SetOwnership(myStyle, False )
+        ROOT.SetOwnership(myStyle, False)
         ROOT.gROOT.SetStyle(myStyle.GetName())
         del myStyle
-    
+
     def __exit__(self, type, value, traceback):
+        # Restore the old style
         ROOT.gROOT.SetStyle(self.old_style)
 
